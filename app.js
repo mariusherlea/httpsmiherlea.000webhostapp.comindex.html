9ccd92c923fff72a2b176caaa8b52d8a094c14f3
Marius <mariusherlea@gmail.com>
2022-11-20 11:52:22 +0100

    buton in id main header fix age

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const d = new Date();
-document.getElementById("age").innerHTML = d.getFullYear() - 1977;
-
 function curentYear() {
-  document.getElementById("curentYear").innerHTML = d.getFullYear();
+  document.getElementById("curentYear").innerHTML = new Date().getFullYear();
+  const para = document.createElement("p");
+  para.innerHTML = `<button>Cacac</button>`;
+  document.getElementById("main-header").appendChild(para);
 }
+
+// const para = document.createElement("p");
+// para.innerText = "Some bullshit";
+// document.body.appendChild(para);
